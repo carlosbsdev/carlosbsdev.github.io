e66fc7cb409be71dd54b05f1ca80a0c3e70221dc
Carlos_ <carlos11811@gmail.com>
2016-12-16 13:51:10 +0100

    Update advertisement.js

--- advertisement.js
+++ advertisement.js
@@ -1,3 +1,1 @@
-window.onload = function() {
-  document.getElementsByTagName("body")[0].innerHTML += "<div id='adblock' style='display:none;'></div>";
-};
+ document.getElementsByTagName("body")[0].innerHTML += "<div id='adblock' style='display:none;'></div>";
